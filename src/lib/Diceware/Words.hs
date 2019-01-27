93f8c4ee00f9cfdaac7db86869a0bf2dfd4f5be2
Dino Morelli <dino@ui3.info>
2019-01-27 11:08:19 -0500

    Changed (IntMap String) to (IntMap Text)

--- src/lib/Diceware/Words.hs
+++ src/lib/Diceware/Words.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Diceware.Words
   ( Dicemap
   , listToKeyInt
@@ -11,29 +13,47 @@ import Data.Char ( isDigit )
 import Data.IntMap.Lazy ( IntMap )
 import qualified Data.IntMap.Lazy as IntMap
 import Data.Maybe ( fromJust, isJust )
-import Safe ( readMay, tailDef )
+import Data.Text ( Text )
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Data.Text.Read ( decimal )
 import System.FilePath ( FilePath )
 
 
-type Dicemap = IntMap String
+type Dicemap = IntMap Text
 
 
 loadWordlist :: FilePath -> IO Dicemap
 loadWordlist dicewareWordlistPath = do
-  wordlistLines <- lines <$> readFile dicewareWordlistPath
+  wordlistLines <- T.lines <$> T.readFile dicewareWordlistPath
   let parsed =
         -- 3. Remove any with left sides that didn't read successfully
         map (first fromJust) . filter (isJust . fst)
         -- 2. Turn the left side string of each tuple into a (Maybe Int)
         . map (first readMay)
         -- 1. Separate each line into the digits string and the word
-        . map (takeWhile isDigit &&& tailDef "" . dropWhile isDigit)
+        . map (T.takeWhile isDigit &&& tailDef "" . T.dropWhile isDigit)
         $ wordlistLines
   return $ IntMap.fromList parsed
 
 
+tailDef :: Text -> Text -> Text
+tailDef def ""  = def
+tailDef _   txt = T.tail txt
+
+
+readMay :: Integral a => Text -> Maybe a
+readMay txt = case decimal txt of
+  Right (int, "") -> Just int
+  _               -> Nothing
+
+
+{-
+  Performing the Data.Text.unpack here instead of in the caller.
+-}
+
 lookupWord :: IntMap.Key -> Dicemap -> Maybe String
-lookupWord = IntMap.lookup
+lookupWord key dicemap = T.unpack <$> IntMap.lookup key dicemap
 
 
 listToKeyInt :: [Int] -> Int

b3185465b62ea6d9b94e447d33afd8cd7a056605
Dino Morelli <dino@ui3.info>
2019-01-28 11:10:38 -0500

    Now using optparse-applicative for args parsing
    This also changed how we handle the arguments in pickWords, getting them
    out of an Options data structure instead of a raw args [String]. Some
    help info was also moved from the main source file into usage strings.

--- src/diceware-auto/Main.hs
+++ src/diceware-auto/Main.hs
@@ -1,28 +1,18 @@
-{-
-  This software was inspired by The Diceware Passphrase Home Page
-  http://world.std.com/~reinhold/diceware.html
--}
-
 import Control.Monad (replicateM, replicateM_)
 import Data.List (intercalate)
-import System.Environment ( getArgs )
 import System.FilePath ( (</>), (<.>), FilePath )
 import Text.Printf ( printf )
 
 import Diceware.Math ( calculateEntropy )
 import Diceware.Random ( generateRandomDieRoll )
 import Diceware.Words ( Dicemap, size, listToKeyInt, loadWordlist, lookupWord )
+import Options
 
 
 dicewareWordlistPath :: FilePath
 -- FIXME This isn't going to live here forever
 dicewareWordlistPath = "util" </> "resources" </> "diceware" <.> "wordlist" <.> "asc"
 -- dicewareWordlistPath = "diceware.wordlist.asc"
-
-
-defaultNumWords, defaultNumLines :: String
-defaultNumWords = "8"
-defaultNumLines = "30"
 
 
 {-
@@ -42,31 +32,24 @@ getWord dicemap = do
   Pick lists of lists of diceware words given a number of words per line
   and a number of lines
 -}
-pickWords :: [String] -> IO ()
-
-pickWords (numWords : [])            =
-  pickWords [numWords, defaultNumLines]
-
-pickWords (numWordsS : numLinesS : []) = do
+pickWords :: Options -> IO ()
+pickWords opts = do
   mapWordlist <- loadWordlist dicewareWordlistPath
 
   let wordListSize = size mapWordlist
   printf "Number of elements (words) in word list: %d\n" wordListSize
-  let numWords = read numWordsS
-  printf "Number of words per passphrase: %d\n" numWords
-  let entropy = calculateEntropy (fromIntegral wordListSize) (fromIntegral numWords)
+  let words' = optWords opts
+  printf "Number of words per passphrase: %d\n" words'
+  let entropy = calculateEntropy (fromIntegral wordListSize) (fromIntegral words')
   printf "Bits of entropy: %.2f\n\n" (entropy :: Double)
 
-  replicateM_ (read numLinesS) $ do
+  replicateM_ (optLines opts) $ do
     -- Generate the words
-    generatedWordList <- replicateM numWords $ getWord mapWordlist
+    generatedWordList <- replicateM words' $ getWord mapWordlist
 
     -- Print it formatted with interspersed spaces
     putStrLn . intercalate " " $ generatedWordList
 
-pickWords _                          =
-  pickWords [defaultNumWords, defaultNumLines]
-
 
 main :: IO ()
-main = getArgs >>= pickWords
+main = parseOptions >>= pickWords
---
+++ src/diceware-auto/Options.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Options
+  ( Options (..)
+  , parseOptions
+  )
+  where
+
+import Data.Version ( showVersion )
+import Options.Applicative
+import Paths_diceware_auto ( version )
+import Text.Heredoc ( here )
+import Text.PrettyPrint.ANSI.Leijen ( string )
+import Text.Printf ( printf )
+
+
+data Options = Options
+  { optWords :: Int
+  , optLines :: Int
+  }
+  deriving Show
+
+
+parser :: Parser Options
+parser = Options
+  <$> option auto
+      (  long "words"
+      <> short 'w'
+      <> help "Number of words to generate per line"
+      <> showDefault
+      <> value 8
+      <> metavar "INT"
+      )
+  <*> option auto
+      (  long "lines"
+      <> short 'l'
+      <> help "Number of lines of words to generate"
+      <> showDefault
+      <> value 30
+      <> metavar "INT"
+      )
+
+
+parseOptions :: IO Options
+parseOptions = execParser $ info (parser <**> helper)
+  (  header "diceware-auto - Randomly generate Diceware passphrases"
+  <> footer'
+  )
+
+
+footer' :: InfoMod a
+footer' = footerDoc . Just . string $ printf content (showVersion version)
+  where content = tail . init $ [here|
+As of 2019-Jan it's recommended to use 96 bits of entropy for your most sensitive passphrases. For Diceware this translates into 8 words in length which is the default value at this time. Bits of entropy will be calculated and displayed during execution. For more information on password strength, see https://en.wikipedia.org/wiki/Password_strength
+
+This software is by-nature less secure than rolling five 6-sided dice and looking up the words in a paper word list. But it's quite a bit more convenient. It does make some small attempt to be safe by using the cryptonite library to generate random values for die rolls.
+
+For more information on the Diceware system of passphrase generation, please see The Diceware Passphrase Home Page http://world.std.com/~reinhold/diceware.html  Here you can also find links to word list files in languages other than English.
+
+Version %s  Dino Morelli <dino@ui3.info>
+|]
